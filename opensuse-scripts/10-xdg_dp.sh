b8ab31143e25f2b083964f892dd5495b1f090280
me-js-bro <me.js.bro@gmail.com>
2025-01-28 19:40:52 +0600

    updated scripts for xdg-desktop-portal

--- opensuse-scripts/10-xdg_dp.sh
+++ opensuse-scripts/10-xdg_dp.sh
@@ -65,13 +65,13 @@ msg att "Checking for other XDG-Desktop-Portal-Implementations..." && sleep 1
 for xdgs in "${removable[@]}"; do
     if sudo zypper se -i "$xdgs" &> /dev/null; then
 
-        fn_ask "Would you like to remove ${cyan}${xdgs}${end}?" "Yes!" "No!"
+        fn_ask "Would you like to remove $xdgs?" "Yes!" "No!"
 
         if [[ $? -eq 0 ]]; then
-            msg act "Removing ${cyan}${xdgs}${end}..."
+            msg act "Removing $xdgs..."
             sudo zypper rm -y "$xdgs" 2>&1 | tee -a "$log"
         else
-            msg skp "Won't remove ${cyan}${xdgs}${end}.."
+            msg skp "Won't remove $xdgs.."
         fi
     fi
 
